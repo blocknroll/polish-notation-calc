8f253aa75b57f91ce89d7f2c74ffbb62662d1c0b
Rachel Warbelow <rachelwarbelow@gmail.com>
2014-12-30 17:21:47 -0700

    initial commit

---
+++ test/polish_notation_calculator_test.rb
@@ -0,0 +1,21 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/polish_notation_calculator'
+
+class PrefixCalculatorTest < Minitest::Test
+  def test_it_exists
+    assert PrefixCalculator
+  end
+
+  def test_it_can_add
+    skip
+  end
+
+  def test_it_can_add_with_a_negative_number
+    skip
+  end
+
+  def test_it_can_subtract
+    skip
+  end
+end
